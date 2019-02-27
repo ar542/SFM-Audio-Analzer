991318fd85d246fe79171aeaaf4c19bf7d3de280
ar542 <ar542@njit.edu>
2019-02-27 15:23:34 -0500

    Update sfmAudio.py

--- sfmAudio.py
+++ sfmAudio.py
@@ -1,14 +1,13 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'C:/Python34/Lib/site-packages/PySide/sfmAudio.ui'
-#
+#autosaver v1.0  
+#BY http://steamcommunity.com/id/OMGTheresABearInMyOatmeal/
+#feel free to modify the script for your own use
 # Created: Mon Feb 25 13:57:52 2019
-#      by: pyside-uic 0.2.15 running on PySide 1.2.4
-#
-# WARNING! All changes made in this file will be lost!
+
 
 from PySide import QtCore, QtGui
-import pyaudio,os,wave,numpy,audioop 
+import wave,numpy,re 
 
 class AudioFileAnalyze(object):
     
@@ -31,10 +30,7 @@ Audio File Info:
  channels: %d
  Rate: %dHz
  BufferSize: %.02f
- Duration: %.02fsec
-
-        
-        
+ Duration: %.02fsec       
         """%(wavefile,self.wf.getnchannels(),self.wf.getframerate(),self.chunk,float(self.wf.getnframes()/self.wf.getframerate() )))
 
         self.progressbarWindow = QtGui.QWidget()
@@ -56,32 +52,33 @@ Audio File Info:
 
         return vs.DmeTime_t(5.00000+(1.0/self.wf.getframerate())*(self.chunk*numofchunks))
 
-    def analyze(self,dataValue,EndFrame=None):
 
 
-
-
-     #   nframes=(self.wf.getnframes()/self.chunk) if  EndFrame==0 else numpy.ceil(float((self.wf.getframerate()/sfmApp.GetFramesPerSecond())*EndFrame)/self.chunk)
-
-
+    def analyze(self,dataValue,duration):
         
 
-        self.progressbar.setMinimum(0)
-       # self.progressbar.setMaximum(nframes*2)
+        self.progressbar.setMinimum(0)       
         self.progressbar.setValue(0)
-
         self.progressbarWindow.show()
 
 
-
+        #reads a single chunk
         data = self.wf.readframes(int(self.chunk))
 
+        #loops though entire wav file
         while data != '':
             self.progressbar.setValue(self.progressbar.value()+1)
+            
+            #temp array holds the data points from the chunk
             dataarray = numpy.fromstring(data,dtype=numpy.int16)
+            
+            #after averaging the array to a single value appends it to the out array
             dataValue.append(float(numpy.average(numpy.abs(dataarray))*2)/2.0**15)   
-            if EndFrame and self.wf.tell()>=(float(self.wf.getframerate()/sfmApp.GetFramesPerSecond())*EndFrame):
-                break             
+            
+            #checks if we hit the endframe
+            if self.wf.tell() >= (float(self.wf.getframerate()/sfmApp.GetFramesPerSecond())*duration):
+                break    
+            
             data = self.wf.readframes(int(self.chunk))
 
 
@@ -89,9 +86,11 @@ Audio File Info:
         self.wf.close()
 
 
-        
+        #not needed for this script but incase someone wants to use it
     def play(self):
         """ Play entire file """
+        import pyaudio
+        
         self.p = pyaudio.PyAudio()
         self.stream = self.p.open(
             format = self.p.get_format_from_width(self.wf.getsampwidth()),
@@ -115,7 +114,7 @@ Audio File Info:
 class Audio_Dialog(object):
     
     
-    ##toDo loop pages add end frame
+   
     
     
     
@@ -125,11 +124,35 @@ class Audio_Dialog(object):
         self.setupUi(self.Dialog)
         self.animset = sfm.GetCurrentAnimationSet()
         self.ExpandToolBox()
-        self.audiofilepath = "C:/Program Files (x86)/Steam/steamapps/common/SourceFilmmaker/game/tf/sound/music/bonus_round_30.wav"
+        self.audiofilepath = None
         self.audioAnalyze = None
+	
+	
+	if  type(sfmClipEditor.GetSelectedClips()[0]) is not vs.movieobjects.CDmeSoundClip:
+	    self.ErrorBox("One Wav file needs to be selected!")
+	    return	
+	ClipName= sfmClipEditor.GetSelectedClips()[0].GetName().replace('\\', '/')
+	
+	
+	#we need to search though sfm file cache because CDmeSoundClip does not hold full file path
+	with open("build_file_cache.txt", 'r') as inF:
+	    for line in inF:
+		if  ClipName in line:
+		    
+		    self.audiofilepath= re.search(r'game.*?\.wav',line).group()[5:]
+		    break          
+
+
+
+
+
+
+
 
         self.Dialog.closeEvent = self.closeEvent
+        #allows user to somewhat interact with sfm while script is active. any changes to scene will happen once script is closed
         self.Dialog.show()
+        #important!!, any gui that needs to alter animset values for animation needs to be .exec_() else sfm will crash
         self.Dialog.exec_()
         
 
@@ -148,26 +171,36 @@ class Audio_Dialog(object):
         font = QtGui.QFont()
         font.setPointSize(10)
         msgBox.setFont(font)
-        msgBox.addButton("&Continue", QtGui.QMessageBox.RejectRole)
+        msgBox.addButton("&Retry", QtGui.QMessageBox.RejectRole)
         if msgBox.exec_() == QtGui.QMessageBox.AcceptRole:pass
         
         
-    def getfile(self):
+      
         
-        self.audiofilepath, foo = QtGui.QFileDialog.getOpenFileName(None,
-                    "Load wav file","", "(*.wav)")    
-        self.filePath_lineedit.setText(self.audiofilepath)        
+    def startAnalyze(self):
         
-    def test(self):
+        if not self.audiofilepath:
+            self.ErrorBox("No Wave file picked!")
+            return
         
-        #if not self.audiofilepath:
-        #    self.ErrorBox("No Wave file picked!")
-        #    return
+           
+        
+        
+        endFramevalue=sfmClipEditor.GetSelectedClips()[0].GetEndTime().ToFractionalFrame(vs.DmeFramerate_t(sfmApp.GetFramesPerSecond()))
+        
+        startFramevalue=sfmClipEditor.GetSelectedClips()[0].GetStartTime().ToFractionalFrame(vs.DmeFramerate_t(sfmApp.GetFramesPerSecond()))
+	
+	
+	
+	
         # Change the operation mode to passthrough so changes chan be made
         # temporarily
         sfm.SetOperationMode("Pass")	
         sfmApp.SetTimelineMode(2)     
         
+        
+        ###loops though all the pages and adds them to the dict if they are enabled
+        ##key=control name: value=[min value,max value]
         controldict={}
         
         for index in range(1,self.control_toolBox.count ()):
@@ -178,63 +211,57 @@ class Audio_Dialog(object):
                 maxvalue= self.control_toolBox.widget(index).findChild(QtGui.QDoubleSpinBox,"doubleSpinBox_maxvalue").value()
                 controldict[self.control_toolBox.itemText(index)]=[minvalue,maxvalue]
 
-      
+        ####
+        
+        if  not controldict:
+            self.ErrorBox("No Controls were Enabled!")
+            return
+
 
         self.Dialog.hide()
+        
+        #inits audio class
         self.audioAnalyze = AudioFileAnalyze(self.audiofilepath,self.spinBox_buffer.value() if self.checkBox_overrideBuffer.isChecked() else None)
-
-        numframes=(self.audioAnalyze.wf.getnframes()/self.audioAnalyze.chunk) if  self.endFrameSpinBox.value()-self.startFrameSpinBox.value()==0 else numpy.ceil(float((self.audioAnalyze.wf.getframerate()/sfmApp.GetFramesPerSecond())*self.endFrameSpinBox.value()-self.startFrameSpinBox.value())/self.audioAnalyze.chunk)
         
 
+
+        #used to calulate the number of chunks ,for progressbar
+	
+        numframes=(numpy.ceil(float((self.audioAnalyze.wf.getframerate()/sfmApp.GetFramesPerSecond())*(endFramevalue-startFramevalue))/self.audioAnalyze.chunk))
+        
+        
+        #set max for progressbar
         self.audioAnalyze.progressbar.setMaximum(numframes*2*len(controldict.keys()))
 
 
+        #fill up array with the data
         dataValue = []
-        self.audioAnalyze.analyze(dataValue,self.endFrameSpinBox.value()-self.startFrameSpinBox.value())#fill up the array with data points
+        self.audioAnalyze.analyze(dataValue,endFramevalue-startFramevalue)#fill up the array with data points
        
 
-
+        #loop though the dict 
         for control in controldict.keys():
 
-            print type(control)
+            #loop though data array
             for index in range(len(dataValue)):
-
+                
                 self.AddKeyFrame(str(control),
-                                 vs.DmeTime_t((self.startFrameSpinBox.value() * (1.0 / sfmApp.GetFramesPerSecond()))) + self.audioAnalyze.getBufferFrameTime(numofchunks=index + 1),
+                                 vs.DmeTime_t((startFramevalue * (1.0 / sfmApp.GetFramesPerSecond()))) + self.audioAnalyze.getBufferFrameTime(numofchunks=index + 1),
                                  dataValue[index],controldict[control][0],controldict[control][1])
+                
                 self.audioAnalyze.progressbar.setValue(self.audioAnalyze.progressbar.value() + 1)
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-        
-       # sound=AudioFile("C:/Program Files
-       # (x86)/Steam/steamapps/common/SourceFilmmaker/game/tf/sound/music/bonus_round_30.wav")
-        
-      #  sound.play(self.AddKeyFrame,"verticalFOV")
-       
-        #print
-        #self.comboBox_animset.itemData(self.comboBox_animset.currentIndex() )
-     #   print
-     #   self.control_toolBox.widget(2).findChild(QtGui.QGroupBox).isChecked()
-        
-   #     print self.control_toolBox.itemText(2)
         #cleanup
         del self.audioAnalyze
         self.Dialog.accept()
 
         
-        
+        #adds key value that can be clamped
+        #controlName:str
+        #time:vs.DmeTime_t
+        #value:float
     def AddKeyFrame(self,controlName,time,value,low=0.0,high=1.0):
         if self.addBookmarkCheckBox.isChecked():
             self.animset.FindControl(controlName).channel.log.AddBookmark(time,0)	
@@ -301,6 +328,8 @@ class Audio_Dialog(object):
         gridLayout.addWidget(groupBox_control, 0, 0, 1, 1)
         self.control_toolBox.addItem(tempcontrol, str(controlName))       
         
+        
+        #adds a control page for each control slider in the animset
     def ExpandToolBox(self):
         
         for element in self.animset.controls:
@@ -323,19 +352,8 @@ class Audio_Dialog(object):
         self.verticalLayout.setObjectName("verticalLayout")
         self.gridLayout = QtGui.QGridLayout()
         self.gridLayout.setObjectName("gridLayout")
-        self.fileLabel = QtGui.QLabel(Dialog)
-        self.fileLabel.setObjectName("fileLabel")
-        self.gridLayout.addWidget(self.fileLabel, 0, 0, 1, 1)
-        self.getfileButton = QtGui.QToolButton(Dialog)
-        self.getfileButton.setObjectName("getfileButton")
-        self.gridLayout.addWidget(self.getfileButton, 0, 2, 1, 1)
-        self.filePath_lineedit = QtGui.QLineEdit(Dialog)
-        self.filePath_lineedit.setEnabled(False)
-        self.filePath_lineedit.setFrame(True)
-        self.filePath_lineedit.setDragEnabled(True)
-        self.filePath_lineedit.setReadOnly(True)
-        self.filePath_lineedit.setObjectName("filePath_lineedit")
-        self.gridLayout.addWidget(self.filePath_lineedit, 0, 1, 1, 1)
+	
+
         self.verticalLayout.addLayout(self.gridLayout)
         self.scrollArea = QtGui.QScrollArea(Dialog)
         self.scrollArea.setFrameShape(QtGui.QFrame.NoFrame)
@@ -364,42 +382,44 @@ class Audio_Dialog(object):
         self.page_bright.setObjectName("page_bright")
         self.horizontalLayout = QtGui.QHBoxLayout(self.page_bright)
         self.horizontalLayout.setObjectName("horizontalLayout")
-        self.label_2 = QtGui.QLabel(self.page_bright)
-        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Maximum, QtGui.QSizePolicy.Preferred)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.label_2.sizePolicy().hasHeightForWidth())
-        self.label_2.setSizePolicy(sizePolicy)
-        self.label_2.setObjectName("label_2")
-        self.horizontalLayout.addWidget(self.label_2)
-        self.startFrameSpinBox = QtGui.QSpinBox(self.page_bright)
-        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Maximum, QtGui.QSizePolicy.Fixed)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.startFrameSpinBox.sizePolicy().hasHeightForWidth())
-        self.startFrameSpinBox.setSizePolicy(sizePolicy)
-        self.startFrameSpinBox.setAccelerated(True)
-        self.startFrameSpinBox.setCorrectionMode(QtGui.QAbstractSpinBox.CorrectToNearestValue)
-        self.startFrameSpinBox.setMaximum(999999999)
-        self.startFrameSpinBox.setObjectName("startFrameSpinBox")
-        self.horizontalLayout.addWidget(self.startFrameSpinBox)
-        self.label = QtGui.QLabel(self.page_bright)
-        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Maximum, QtGui.QSizePolicy.Preferred)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.label.sizePolicy().hasHeightForWidth())
-        self.label.setSizePolicy(sizePolicy)
-        self.label.setObjectName("label")
-        self.horizontalLayout.addWidget(self.label)
-        self.endFrameSpinBox = QtGui.QSpinBox(self.page_bright)
-        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Fixed, QtGui.QSizePolicy.Fixed)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.endFrameSpinBox.sizePolicy().hasHeightForWidth())
-        self.endFrameSpinBox.setSizePolicy(sizePolicy)
-        self.endFrameSpinBox.setMaximum(99999999)
-        self.endFrameSpinBox.setObjectName("endFrameSpinBox")
-        self.horizontalLayout.addWidget(self.endFrameSpinBox)
+	
+        #self.label_2 = QtGui.QLabel(self.page_bright)
+        #sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Maximum, QtGui.QSizePolicy.Preferred)
+        #sizePolicy.setHorizontalStretch(0)
+        #sizePolicy.setVerticalStretch(0)
+        #sizePolicy.setHeightForWidth(self.label_2.sizePolicy().hasHeightForWidth())
+        #self.label_2.setSizePolicy(sizePolicy)
+        #self.label_2.setObjectName("label_2")
+        #self.horizontalLayout.addWidget(self.label_2)
+        #self.startFrameSpinBox = QtGui.QSpinBox(self.page_bright)
+        #sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Maximum, QtGui.QSizePolicy.Fixed)
+        #sizePolicy.setHorizontalStretch(0)
+        #sizePolicy.setVerticalStretch(0)
+        #sizePolicy.setHeightForWidth(self.startFrameSpinBox.sizePolicy().hasHeightForWidth())
+        #self.startFrameSpinBox.setSizePolicy(sizePolicy)
+        #self.startFrameSpinBox.setAccelerated(True)
+        #self.startFrameSpinBox.setCorrectionMode(QtGui.QAbstractSpinBox.CorrectToNearestValue)
+        #self.startFrameSpinBox.setMaximum(999999999)
+        #self.startFrameSpinBox.setObjectName("startFrameSpinBox")
+        #self.horizontalLayout.addWidget(self.startFrameSpinBox)
+        #self.label = QtGui.QLabel(self.page_bright)
+        #sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Maximum, QtGui.QSizePolicy.Preferred)
+        #sizePolicy.setHorizontalStretch(0)
+        #sizePolicy.setVerticalStretch(0)
+        #sizePolicy.setHeightForWidth(self.label.sizePolicy().hasHeightForWidth())
+        #self.label.setSizePolicy(sizePolicy)
+        #self.label.setObjectName("label")
+        #self.horizontalLayout.addWidget(self.label)
+        #self.endFrameSpinBox = QtGui.QSpinBox(self.page_bright)
+        #sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Fixed, QtGui.QSizePolicy.Fixed)
+        #sizePolicy.setHorizontalStretch(0)
+        #sizePolicy.setVerticalStretch(0)
+        #sizePolicy.setHeightForWidth(self.endFrameSpinBox.sizePolicy().hasHeightForWidth())
+        #self.endFrameSpinBox.setSizePolicy(sizePolicy)
+        #self.endFrameSpinBox.setMaximum(99999999)
+        #self.endFrameSpinBox.setObjectName("endFrameSpinBox")
+        #self.horizontalLayout.addWidget(self.endFrameSpinBox)
+	
         self.line = QtGui.QFrame(self.page_bright)
         self.line.setFrameShape(QtGui.QFrame.VLine)
         self.line.setFrameShadow(QtGui.QFrame.Sunken)
@@ -429,11 +449,16 @@ class Audio_Dialog(object):
         self.horizontalLayout.addWidget(self.checkBox_overrideBuffer)
         self.spinBox_buffer = QtGui.QSpinBox(self.page_bright)
         self.spinBox_buffer.setEnabled(False)
-        self.spinBox_buffer.setMinimum(32)
+        self.spinBox_buffer.setMinimum(2)
         self.spinBox_buffer.setMaximum(32768)
         self.spinBox_buffer.setSingleStep(1)
         self.spinBox_buffer.setProperty("value", 2048)
         self.spinBox_buffer.setObjectName("spinBox_buffer")
+	sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Maximum, QtGui.QSizePolicy.Fixed)
+	sizePolicy.setHorizontalStretch(0)
+	sizePolicy.setVerticalStretch(0)
+	sizePolicy.setHeightForWidth(self.spinBox_buffer.sizePolicy().hasHeightForWidth())
+	self.spinBox_buffer.setSizePolicy(sizePolicy)	
         self.horizontalLayout.addWidget(self.spinBox_buffer)
         self.control_toolBox.addItem(self.page_bright, "")
         #
@@ -453,9 +478,9 @@ class Audio_Dialog(object):
         self.control_toolBox.layout().setSpacing(8)
 
         QtCore.QObject.connect(self.checkBox_overrideBuffer, QtCore.SIGNAL("toggled(bool)"), self.spinBox_buffer.setEnabled)
-        QtCore.QObject.connect(self.getfileButton, QtCore.SIGNAL("clicked()"), self.getfile)
-        QtCore.QObject.connect(self.buttonBox, QtCore.SIGNAL("accepted()"), self.test)
-        QtCore.QObject.connect(self.buttonBox, QtCore.SIGNAL("rejected()"), Dialog.reject)
+       
+        QtCore.QObject.connect(self.buttonBox, QtCore.SIGNAL("accepted()"), self.startAnalyze)
+        
         QtCore.QMetaObject.connectSlotsByName(Dialog)
 
 
@@ -478,13 +503,11 @@ class Audio_Dialog(object):
     def retranslateUi(self, Dialog):
         Dialog.setWindowTitle(QtGui.QApplication.translate("Dialog", "Dialog", None, QtGui.QApplication.UnicodeUTF8))
 
-        self.fileLabel.setText(QtGui.QApplication.translate("Dialog", "wave file", None, QtGui.QApplication.UnicodeUTF8))
-        self.getfileButton.setText(QtGui.QApplication.translate("Dialog", "...", None, QtGui.QApplication.UnicodeUTF8))
         self.MiscBox.setTitle(QtGui.QApplication.translate("Dialog", "light control options", None, QtGui.QApplication.UnicodeUTF8))
-        self.label_2.setText(QtGui.QApplication.translate("Dialog", "Start Frame", None, QtGui.QApplication.UnicodeUTF8))
+        #self.label_2.setText(QtGui.QApplication.translate("Dialog", "Start Frame", None, QtGui.QApplication.UnicodeUTF8))
         self.addBookmarkCheckBox.setText(QtGui.QApplication.translate("Dialog", "Add Bookmarks", None, QtGui.QApplication.UnicodeUTF8))
         self.control_toolBox.setItemText(self.control_toolBox.indexOf(self.page_bright), QtGui.QApplication.translate("Dialog", "Misc", None, QtGui.QApplication.UnicodeUTF8))
-        self.label.setText(QtGui.QApplication.translate("Dialog", "End Frame", None, QtGui.QApplication.UnicodeUTF8))
+        #self.label.setText(QtGui.QApplication.translate("Dialog", "End Frame", None, QtGui.QApplication.UnicodeUTF8))
         self.checkBox_overrideBuffer.setText(QtGui.QApplication.translate("Dialog", "override BufferSize", None, QtGui.QApplication.UnicodeUTF8))
     
 
